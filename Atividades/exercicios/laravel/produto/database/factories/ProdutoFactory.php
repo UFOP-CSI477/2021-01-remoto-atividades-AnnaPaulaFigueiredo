aa718e5b608b085c0857f5c26d881750e1418d2c
Anna Paula <annapaulafigueiredoo@gmail.com>
2021-11-21 20:15:29 -0300

    create laravel, upload laravel + migrations + factory, ainda falta o exercicio 09, vou na monitoria.

---
+++ Atividades/exercicios/laravel/produto/database/factories/ProdutoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProdutoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nome' => $this->faker->word,
+            'qnt' => $this->faker->randomDigit,
+            'descricao' => $this->faker->company,
+           
+        ];
+    }
+}
